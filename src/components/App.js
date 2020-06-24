f11e7b77efd344e678a2a889206ee705c35533ed
namanmanchanda09 <naman.mohan09@outlook.com>
2020-06-24 14:48:33 +0530

    added Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-// import logo from '../logo.svg';
 import '../styles/App.css';
+import {Switch, Route} from 'react-router-dom';
 import LinkList from "./LinkList";
-// import CreateLink from "./CreateLink";
+import CreateLink from "./CreateLink";
+import Header from './Header'
 
 class App extends React.Component{
     render(){
         return (
-            <LinkList />
+            <div className="center w85">
+                <Header />
+                <div className='ph3 pv1 background-gray'>
+                    <Switch>
+                        <Route exact path='/' component={LinkList}/>
+                        <Route exact path='/create' component = {CreateLink}/>
+                    </Switch>
+                </div>
+            </div>
+
         )
     }
 }
--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -37,7 +37,7 @@ class CreateLink extends React.Component {
                         placeholder="The URL for the link"
                     />
                 </div>
-                <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
+                <Mutation mutation={POST_MUTATION} variables={{ description, url }} onCompleted={()=>this.props.history.push('/')}>
                     {postMutation => <button onClick={postMutation}>Submit</button>}
                 </Mutation>
             </div>
