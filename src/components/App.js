52470afb5334b9da50337981d27ec0e4399eec8a
namanmanchanda09 <naman.mohan09@outlook.com>
2020-06-24 13:37:38 +0530

    added Queries and Mutation to Database

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import logo from '../logo.svg';
 import '../styles/App.css';
 import LinkList from "./LinkList";
+// import CreateLink from "./CreateLink";
 
 class App extends React.Component{
     render(){
